7a5d519cac6d4199a0fc4597628c7ccd932ed4f0
sannahan <73829699+sannahan@users.noreply.github.com>
2021-03-25 13:26:43 +0200

    Tehtävä 1

---
+++ viikko3/tehtavat1-3/nhlreader/src/main/java/ohtu/Player.java
@@ -0,0 +1,74 @@
+
+package ohtu;
+
+public class Player {
+    private String name;
+    private String nationality;
+    private int assists;
+    private int goals;
+    private int penalties;
+    private String team;
+    private int games;
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public int getGames() {
+        return games;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public int getPenalties() {
+        return penalties;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public void setGames(int games) {
+        this.games = games;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPenalties(int penalties) {
+        this.penalties = penalties;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    @Override
+    public String toString() {
+        return name + " team: " + team + " goals: " + goals + " assists: " + assists;
+    }
+      
+}
